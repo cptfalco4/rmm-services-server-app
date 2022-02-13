95b8dac9ba28ce5e79e317891a94326532aa0b6a
nef13 <nicole_falco@aol.com>
2022-02-13 13:39:57 -0500

    Calculations Service

--- src/main/java/com/ninjarmm/rmmservicesserverapp/services/DealCalculationsService.java
+++ src/main/java/com/ninjarmm/rmmservicesserverapp/services/DealCalculationsService.java
@@ -1,8 +1,54 @@
 package com.ninjarmm.rmmservicesserverapp.services;
 
+import com.ninjarmm.rmmservicesserverapp.model.costs.CustomerServiceCost;
+import com.ninjarmm.rmmservicesserverapp.model.devices.DeviceDto;
+import com.ninjarmm.rmmservicesserverapp.model.devices.DeviceType;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class DealCalculationsService {
     public static final int DEVICE_FLAT_PRICE = 4;
+
+    private final DeviceService deviceService;
+    private final CustomerServicesService customerServicesService;
+
+    public DealCalculationsService(DeviceService deviceService,
+                                   CustomerServicesService customerServicesService) {
+        this.deviceService = deviceService;
+        this.customerServicesService = customerServicesService;
+    }
+
+    public int calculateBillForCustomer(String customerId) {
+        Set<DeviceDto> devicesByCustomerId = deviceService.getDevicesByCustomerId(customerId);
+        List<CustomerServiceCost> serviceCosts = customerServicesService.getServiceCostsByCustomerId(customerId);
+        Integer servicesCostAggregated = serviceCosts.stream()
+                .map(customerServiceCost -> multiplyServicesCost(customerServiceCost, devicesByCustomerId))
+                .reduce(0, Math::addExact);
+
+        int generalDevicesCost = Math.multiplyExact(devicesByCustomerId.size(), DEVICE_FLAT_PRICE);
+
+        return Math.addExact(servicesCostAggregated, generalDevicesCost);
+    }
+
+    private int multiplyServicesCost(CustomerServiceCost customerServiceCost, Set<DeviceDto> devicesByCustomerId) {
+        Set<DeviceDto> macDevices = devicesByCustomerId.stream()
+                .filter(deviceDto -> deviceDto.getType().equals(DeviceType.MAC.getName())).collect(Collectors.toSet());
+        Set<DeviceDto> windowsDevices = devicesByCustomerId.stream()
+                .filter(deviceDto -> !deviceDto.getType().equals(DeviceType.MAC.getName())).collect(Collectors.toSet());
+
+        int calculation = 0;
+        switch(customerServiceCost.getServiceName()) {
+            case "Antivirus Mac": calculation = Math.multiplyExact(customerServiceCost.getPrice(), macDevices.size()); break;
+            case "Antivirus Windows" :  calculation = Math.multiplyExact(customerServiceCost.getPrice(), windowsDevices.size()); break;
+            default: calculation = Math.multiplyExact(customerServiceCost.getPrice(), devicesByCustomerId.size()); break;
+        }
+
+        return calculation;
+    }
+
+
 }
---
+++ src/test/java/com/ninjarmm/rmmservicesserverapp/services/DealCalculationsServiceTest.java
@@ -0,0 +1,78 @@
+package com.ninjarmm.rmmservicesserverapp.services;
+
+import com.ninjarmm.rmmservicesserverapp.model.costs.CustomerServiceCost;
+import com.ninjarmm.rmmservicesserverapp.model.devices.DeviceDto;
+import com.ninjarmm.rmmservicesserverapp.model.devices.DeviceType;
+import com.ninjarmm.rmmservicesserverapp.model.services.ServiceName;
+import com.ninjarmm.rmmservicesserverapp.repositories.DeviceRepository;
+import com.ninjarmm.rmmservicesserverapp.repositories.ServiceRepository;
+import org.assertj.core.util.Sets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class DealCalculationsServiceTest {
+    @Mock
+    private DeviceService deviceService;
+    @Mock
+    private CustomerServicesService customerServicesService;
+
+    private DealCalculationsService testObject;
+
+    @BeforeEach
+    public void setUp(){
+        testObject = new DealCalculationsService(deviceService, customerServicesService);
+    }
+
+    @Test
+    public void calculateBillForCustomer(){
+        DeviceDto windowsDevice1 = DeviceDto.builder()
+                .deviceId("device1")
+                .systemName("system1")
+                .type(DeviceType.WINDOWS_WORKSTATION.getName())
+                .build();
+        DeviceDto windowsDevice2 = DeviceDto.builder()
+                .deviceId("device2")
+                .systemName("system1")
+                .type(DeviceType.WINDOWS_WORKSTATION.getName())
+                .build();
+
+        DeviceDto macDevice1 = DeviceDto.builder()
+                .deviceId("device3")
+                .systemName("system1")
+                .type(DeviceType.MAC.getName())
+                .build();
+        DeviceDto macDevice2 = DeviceDto.builder()
+                .deviceId("device4")
+                .systemName("system1")
+                .type(DeviceType.MAC.getName())
+                .build();
+        DeviceDto macDevice3 = DeviceDto.builder()
+                .deviceId("device5")
+                .systemName("system1")
+                .type(DeviceType.MAC.getName())
+                .build();
+
+        String customer1 = "customer1";
+        given(deviceService.getDevicesByCustomerId(customer1))
+               .willReturn(Sets.newHashSet(Arrays.asList(windowsDevice1, windowsDevice2, macDevice1, macDevice2, macDevice3)));
+
+        CustomerServiceCost customerServiceCost1 = new CustomerServiceCost(customer1, ServiceName.ANTIVIRUS_WINDOWS.getName(),5);
+        CustomerServiceCost customerServiceCost2 = new CustomerServiceCost(customer1, ServiceName.ANTIVIRUS_MAC.getName(),7);
+        CustomerServiceCost customerServiceCost3 = new CustomerServiceCost(customer1, ServiceName.CLOUDBERRY.getName(),3);
+        CustomerServiceCost customerServiceCost4 = new CustomerServiceCost(customer1, ServiceName.TEAM_VIEWER.getName(),1);
+
+       given(customerServicesService.getServiceCostsByCustomerId(customer1))
+               .willReturn(Arrays.asList(customerServiceCost1, customerServiceCost2, customerServiceCost3, customerServiceCost4));
+
+       assertEquals(71, testObject.calculateBillForCustomer(customer1));
+    }
+}
